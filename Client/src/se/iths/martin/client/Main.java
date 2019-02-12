7950f2675f064e7df254ebe202457014de452fea
Martin Blomberg <martin.blomberg@outlook.com>
2019-02-12 10:42:22 +0100

    Dynamic loading of services with program argument to specify path to search.

--- Client/src/se/iths/martin/client/Main.java
+++ Client/src/se/iths/martin/client/Main.java
@@ -2,6 +2,11 @@ package se.iths.martin.client;
 
 import se.iths.martin.api.Greetings;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ServiceLoader;
 
 public class Main {
@@ -9,25 +14,30 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.run();
+        main.run(args);
     }
 
-    private void run() {
+    private void run(String[] args) {
+
+        String fileLocation = args[0];
+        File loc = new File(fileLocation);
+
+        File[] flist = loc.listFiles(new FileFilter() {
+            public boolean accept(File file) {return file.getPath().toLowerCase().endsWith(".jar");}
+        });
+        URL[] urls = new URL[flist.length];
+        for (int i = 0; i < flist.length; i++) {
+            try {
+                urls[i] = flist[i].toURI().toURL();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+        URLClassLoader ucl = new URLClassLoader(urls);
 
         ServiceLoader<Greetings> loader =
-                ServiceLoader.load(Greetings.class);
+                ServiceLoader.load(Greetings.class, ucl);
 
-        for (Greetings greetings : loader) {
-            greetings.printYourGreeting();
-            System.out.println(greetings.calculate(2, 3));
-        }
-
-        for (Greetings greetings : loader) {
-            greetings.printYourGreeting();
-            System.out.println(greetings.calculate(2, 3));
-        }
-
-        loader = ServiceLoader.load(Greetings.class);
         for (Greetings greetings : loader) {
             greetings.printYourGreeting();
             System.out.println(greetings.calculate(2, 3));
